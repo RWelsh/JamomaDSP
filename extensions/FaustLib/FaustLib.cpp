f1efbd63bbf381a139fbf04932153b22dce71136
@mr_nilson <peters.nils@gmx.net>
2011-07-04 01:24:09 -0700

    faust extension can now be found by the MaxWrappers

--- extensions/FaustLib/FaustLib.cpp
+++ extensions/FaustLib/FaustLib.cpp
@@ -14,7 +14,7 @@
 #include "TTWah.h"
 #include "TTSmoothDelay.h"
 
-extern "C" TT_EXTENSION_EXPORT TTErr loadTTExtension(void)
+extern "C" TT_EXTENSION_EXPORT TTErr TTLoadJamomaExtension_FaustLib(void)
 {
 	TTDSPInit();
 	
